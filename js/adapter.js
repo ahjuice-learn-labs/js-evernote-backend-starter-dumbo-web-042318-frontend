82aacdaa944cbce3b865b26090af34dc79f9faad
Daniel Shimon <danielzshimon@gmail.com>
2018-06-12 15:24:34 -0400

    added note lists

--- js/adapter.js
+++ js/adapter.js
@@ -1,7 +1,15 @@
 const Adapter = {
   baseUrl: "http://localhost:3000/api/v1/",
-  getAllUsers: function getAllUsers() {
+  getUsers: function getUsers() {
     return fetch(`${Adapter.baseUrl}users`)
       .then(resp => resp.json())
+  },
+
+  getUser: function getUser(id){
+    return fetch(`${Adapter.baseUrl}users/${id}`)
+      .then(resp => resp.json())
   }
+
+
+
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  Adapter.getAllUsers();
+  renderNoteList()
+
+
 });
+
+function renderNotePreview(note) {
+  const noteList = document.querySelector('#note-list');
+  const li = document.createElement('li');
+  li.innerHTML = note.body;
+  noteList.appendChild(li);
+};
+
+function renderNoteList() {
+  let user = Adapter.getUser(1);
+  let userNotes = user.then(r => r.notes)
+  userNotes.then(notes => notes.forEach(note => {
+    renderNotePreview(note)
+  }))
+};
